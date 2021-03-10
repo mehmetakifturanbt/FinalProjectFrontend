bab21658bf8dd3a67a839b50e632d64fd8e846c4
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-03-11 01:21:41 +0300

    10 Mart Gün Sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,10 +9,19 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
- 
-  products:Product[] = [];
+  products: Product[] = [];
+  dataLoaded = false;
 
-  constructor() {}
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
 }
