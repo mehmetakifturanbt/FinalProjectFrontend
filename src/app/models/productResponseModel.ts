bd78be8c1b65b7fb37b738ddb4862797262f9db4
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-03-10 23:47:01 +0300

    10 Mart Commit2

---
+++ src/app/models/productResponseModel.ts
@@ -0,0 +1,9 @@
+import { Product } from "./product";
+import { ResponseModel } from "./responseModel";
+
+export interface ProductResponseModel extends ResponseModel{
+
+    data:Product[],
+    
+
+}
