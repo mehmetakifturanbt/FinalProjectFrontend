64419a56c94a3ac3bb75ccdea32406d61745662c
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-03-18 14:48:11 +0300

    17 Mart Ders Sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,7 +15,7 @@ export class ProductComponent implements OnInit {
   dataLoaded = false;
   filterText="";
 
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) {}
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private toastrService:ToastrService, private cartService:CartService) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -37,4 +39,15 @@ export class ProductComponent implements OnInit {
       this.dataLoaded = true;
     });
   }
+
+  addToCart(product:Product){
+    if(product.productId===1){
+      this.toastrService.error("Hata","Bu ürün sepete eklenemez")
+    }else{
+      this.toastrService.success("Sepete eklendi",product.productName)
+      this.cartService.addToCart(product);
+
+    }
+    
+  } 
 }
